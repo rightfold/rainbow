5cf9625b0dfe8a4a46dbc3cc4a59b4cf4b0ad2a6
rightfold <rightfold@gmail.com>
2016-05-14 22:33:15 +0200

    Parse symbols, lists, and arrays

--- rainbow/read.rkt
+++ rainbow/read.rkt
@@ -1,22 +1,44 @@
 #lang racket
-(provide lex)
+(provide lex parse)
 (require
   (prefix-in lex- parser-tools/lex)
   (prefix-in re- parser-tools/lex-sre)
+  (prefix-in yacc- parser-tools/yacc)
 )
 
 (lex-define-tokens tokens (SYMBOL))
-(lex-define-empty-tokens empty-tokens (LPAREN RPAREN LBRACKET RBRACKET))
+(lex-define-empty-tokens empty-tokens (EOF LPAREN RPAREN LBRACKET RBRACKET))
 
 (define lex
   (lex-lexer
     (lex-whitespace (lex lex-input-port))
     ((re-+ (re-or lex-alphabetic "-" "?" "!"))
-     (token-SYMBOL lex-lexeme))
+     (token-SYMBOL (string->symbol lex-lexeme)))
     ("(" (token-LPAREN))
     (")" (token-RPAREN))
     ("[" (token-LBRACKET))
     ("]" (token-RBRACKET))
+    ((eof) (token-EOF))
+  )
+)
+
+(define parse
+  (yacc-parser
+    (start sexprs)
+    (end EOF)
+    (error void)
+    (tokens tokens empty-tokens)
+    (grammar
+      (sexprs
+        (()             '())
+        ((sexpr sexprs) (cons $1 $2))
+      )
+      (sexpr
+        ((SYMBOL) $1)
+        ((LPAREN sexprs RPAREN) $2)
+        ((LBRACKET sexprs RBRACKET) (list->vector $2))
+      )
+    )
   )
 )
 
@@ -28,15 +50,25 @@
       (for ((token tokens))
         (check-equal? (lex port) token)
       )
-      (check-equal? (lex port) 'eof)
+      (check-equal? (lex port) (token-EOF))
     )
   )
 
   (test-lex "" (list))
-  (test-lex "a" (list (token-SYMBOL "a")))
-  (test-lex "ab" (list (token-SYMBOL "ab")))
-  (test-lex "ab cd" (list (token-SYMBOL "ab") (token-SYMBOL "cd")))
+  (test-lex "a" (list (token-SYMBOL 'a)))
+  (test-lex "ab" (list (token-SYMBOL 'ab)))
+  (test-lex "ab cd" (list (token-SYMBOL 'ab) (token-SYMBOL 'cd)))
   (test-lex "()" (list (token-LPAREN) (token-RPAREN)))
-  (test-lex "(ab)" (list (token-LPAREN) (token-SYMBOL "ab") (token-RPAREN)))
-  (test-lex "[ab]" (list (token-LBRACKET) (token-SYMBOL "ab") (token-RBRACKET)))
+  (test-lex "(ab)" (list (token-LPAREN) (token-SYMBOL 'ab) (token-RPAREN)))
+  (test-lex "[ab]" (list (token-LBRACKET) (token-SYMBOL 'ab) (token-RBRACKET)))
+
+  (define (test-parse text sexprs)
+    (let ((port (open-input-string text)))
+      (check-equal? (parse (lambda () (lex port))) sexprs)))
+
+  (test-parse "" '())
+  (test-parse "a" '(a))
+  (test-parse "a b" '(a b))
+  (test-parse "a () (b)" '(a () (b)))
+  (test-parse "a [] [b]" '(a #[] #[b]))
 )
